348e6daded2641af349a4033855e8388c01cedeb
JavierUrban <59941891+JavierUrban@users.noreply.github.com>
2021-01-06 23:04:50 -0600

    Add files via upload

---
+++ bin/pilon.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+##pilon v1.23##
+##the -frags option is used to use alignment from pair-end sequences
+##and depending on the size of the data it is important to assign the number of ram memory with --Xmx
+mkadir ../results/genome_pulido
+pilon --genome ../results/assembly_genome_lepto.fa.gz --frags ../results/aln_genoma_illu.sorted.bam --output ../results/genome_pulido/ --threads 10 --mingap 8 -Xmx32G
